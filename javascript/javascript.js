6582a5ee0ea2cf74e18b35909fa9cf7cb244c1c7
Youssef <ybelalmarzouk@gmail.com>
2022-12-16 14:32:25 +0200

    feat:add acordion function

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -6,3 +6,15 @@ function show_hide_divs(clickedBtn) {
 	var selectedTab = document.querySelector("#" + clickedBtn);
 	selectedTab.classList.add("active");
 }
+
+function show_hide_answers(clickedBtn_acordion) {
+	var clickedarrow = document.querySelectorAll(".main-idea");
+	for (i = 0; i < clickedarrow.length; i++) {
+		if (clickedarrow[i] != clickedBtn_acordion)
+			clickedarrow[i].classList.remove("active-acordion");
+	}
+	if (clickedBtn_acordion.classList.contains("active-acordion")) {
+		clickedBtn_acordion.classList.remove("active-acordion")
+	}
+	else (clickedBtn_acordion.classList.add("active-acordion"))
+}
